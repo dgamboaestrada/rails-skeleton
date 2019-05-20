e4d428aee36c7c5bc7cdaed5a794a4433df69483
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2019-05-19 20:14:27 -0500

    Add User REST API

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,9 +1,59 @@
 class UsersController < InheritedResources::Base
+  before_action :set_user, only: %i[show update destroy]
+
+  def index
+    @users = User.order(params[:sort]).per_page_kaminari(params[:page]).per params[:limit]
+
+    respond_to do |format|
+      format.json { render json: @users }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.json { render json: @user }
+    end
+  end
+
+  def create
+    @user = User.create(
+      email: user_params[:email],
+      password: user_params[:password],
+    )
+    respond_to do |format|
+      if @user.valid?
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    respond_to do |format|
+      if @user.valid?
+        format.json { render json: @user, status: :ok, location: @user  }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
 
   private
 
-    def user_params
-      params.require(:user).permit()
-    end
-end
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :roles
-  resources :users
+  resources :roles, :path => 'api/v1/roles', :defaults => { :format => :json }
+  resources :users, :path => 'api/v1/users', :defaults => { :format => :json }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
